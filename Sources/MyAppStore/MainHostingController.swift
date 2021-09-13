799ba0e3ce2e7b7836187b2c920a099c5f5d447f
肇鑫（Zhao Xin） <owenzx@gmail.com>
2021-09-13 16:32:02 +0800

    * added new files.

---
+++ Sources/MyAppStore/MainHostingController.swift
@@ -0,0 +1,26 @@
+//
+//  MainHostingController.swift
+//  App Demo
+//
+//  Created by zhaoxin on 2021/8/23.
+//
+
+import Cocoa
+import SwiftUI
+
+class MainHostingController: NSHostingController<MainSwiftUIView> {
+    @objc required dynamic init?(coder: NSCoder) {
+        super.init(coder: coder, rootView: MainSwiftUIView())
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+        if let window = view.window {
+            window.title = NSLocalizedString("Developer Other Apps", bundle: .module, comment: "")
+            var frame = window.frame
+            frame.size = NSSize(width: 800, height: 600)
+            window.setFrame(frame, display: false, animate: false)
+        }
+    }
+}
